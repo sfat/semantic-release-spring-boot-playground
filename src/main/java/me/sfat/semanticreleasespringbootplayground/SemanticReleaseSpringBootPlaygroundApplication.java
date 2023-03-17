66ea818f75804b14b6708cb20b10783bb18d82a4
Andrei Sfat <andrei@sfat.me>
2023-03-17 09:02:27 +0200

    feat: initial commit (#1)

---
+++ src/main/java/me/sfat/semanticreleasespringbootplayground/SemanticReleaseSpringBootPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package me.sfat.semanticreleasespringbootplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanticReleaseSpringBootPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemanticReleaseSpringBootPlaygroundApplication.class, args);
+	}
+
+}
